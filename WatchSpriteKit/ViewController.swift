7248def06031190bcc2a7590f0442248035093c6
Adam Eri <adam.eri@blackmirror.media>
2018-04-08 20:40:08 +0200

    Initial Commit

---
+++ WatchSpriteKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchSpriteKit
+//
+//  Created by Adam Eri on 08.04.18.
+//  Copyright © 2018 blackmirror media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
